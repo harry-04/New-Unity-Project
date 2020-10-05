db016c74b3eb6f0b4eed600fcf2418449921ecb2
Harry Ermidis <510713@escg.ac.uk>
2020-10-05 12:22:21 +0100

    Initial commit

---
+++ Assets/Scripts/Adventurer2Move.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adventurer2Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xSpeed = 5.0f;
+        float ySpeed = 5.0f;
+        if (Input.GetKey("a"))
+        {
+            // registers a key held down and returns true
+
+            xSpeed = -5.0f;
+            transform.position += new Vector3(xSpeed * Time.deltaTime, 0, 0);
+        }
+
+        
+
+        if (Input.GetKey("d"))
+        {
+            // registers a key held down and returns true
+
+            xSpeed = 5.0f;
+            transform.position += new Vector3(xSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb.velocity = new Vector3(0, 10, 0);
+        }
+        return;
+
+        if (Input.GetKey("s"))
+        {
+            // registers a key held down and returns true
+
+            ySpeed = -5.0f;
+            transform.position += new Vector3(0, ySpeed * Time.deltaTime, 0);
+        }
+
+    } 
+}
---
+++ Assets/Scripts/AdventurerMove.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdventurerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Rigidbody2D rb;
+    
+
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xSpeed = 5.0f;
+        float ySpeed = 5.0f;
+        if (Input.GetKey("left"))
+        {
+            // registers a key held down and returns true
+
+            xSpeed = -5.0f;
+            transform.position += new Vector3(xSpeed * Time.deltaTime, 0, 0);
+        }
+
+        
+
+        if (Input.GetKey("right"))
+        {
+            // registers a key held down and returns true
+
+            xSpeed = 5.0f;
+            transform.position += new Vector3(xSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("up"))
+        {
+            rb.velocity = new Vector3(0, 10, 0);
+        }
+        return;
+
+        if (Input.GetKey("down"))
+        {
+            // registers a key held down and returns true
+
+            ySpeed = -5.0f;
+            transform.position += new Vector3(0, ySpeed * Time.deltaTime, 0);
+        }
+
+    } 
+}

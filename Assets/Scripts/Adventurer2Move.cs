4214e7fbe1a87cc848714412ec38aa1da99a9c70
harry-04 <72386526+harry-04@users.noreply.github.com>
2020-10-14 16:18:56 +0100

    sadsad

--- Assets/Scripts/Adventurer2Move.cs
+++ Assets/Scripts/Adventurer2Move.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Adventurer2Move : MonoBehaviour
 {
     // Start is called before the first frame update
+    SpriteRenderer sr;
+    public Sprite jumpSprite;
+    public Sprite standSprite;
     public Rigidbody2D rigidbody2d;
     public BoxCollider2D boxCollider2d;
     //specifies which layers we want to hit with our ray cast (in this case only the "platforms" layer)
@@ -17,10 +20,11 @@ public class Adventurer2Move : MonoBehaviour
         return raycastHit2d.collider != null;
     }
 
-    
-    
+
+
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
         rigidbody2d = transform.GetComponent<Rigidbody2D>();
         boxCollider2d = transform.GetComponent<BoxCollider2D>();
     }
@@ -34,26 +38,30 @@ public class Adventurer2Move : MonoBehaviour
             // registers a key held down and returns true (rigidbody2d.velocity is just a longer version of rb.velocity)
             rigidbody2d.velocity = new Vector2(-moveSpeed, rigidbody2d.velocity.y);
 
-            
+
         }
 
-        
+
 
         if (Input.GetKey(KeyCode.D))
         {
             // registers a key held down and returns true
             rigidbody2d.velocity = new Vector2(+moveSpeed, rigidbody2d.velocity.y);
 
-            
+
         }
 
         if (isGrounded() && Input.GetKeyDown(KeyCode.W))
         {
             float jumpVelocity = 20f;
             rigidbody2d.velocity = Vector2.up * jumpVelocity;
+            sr.sprite = jumpSprite;
         }
         return;
 
-
-    } 
+        if (rigidbody2d.velocity = new Vector2(0 + 0)
+        {
+            sr.sprite = jumpSprite;
+        }
+    }
 }
